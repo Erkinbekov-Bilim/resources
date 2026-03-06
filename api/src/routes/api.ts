a3b012ec1261f7f42c421d260a0634850f5da8f1
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-03-07 03:19:36 +0600

    refactor(api): rename itemLocations route to locations

--- api/src/routes/api.ts
+++ api/src/routes/api.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import categoriesRouter from './resources/categories.route.js';
 import inventoryRouter from './resources/inventory.route.js';
-import itemLocationsRouter from './resources/itemLocations.route.js';
+import itemLocationsRouter from './resources/locations.route.js';
 
 const apiRouter: Router = Router();
 
 apiRouter.use('/inventory', inventoryRouter);
 apiRouter.use('/categories', categoriesRouter);
-apiRouter.use('/itemLocations', itemLocationsRouter);
+apiRouter.use('/locations', itemLocationsRouter);
 
 export default apiRouter;
