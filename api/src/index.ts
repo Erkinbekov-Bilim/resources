7a30e4c6d1dd36b4c89821fe8cea8f2339a35170
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-03-07 01:53:34 +0600

    feat(api): initialize database connection on app startup

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,14 +3,18 @@ import type { Express } from 'express';
 import cors from 'cors';
 import { PORT } from './constants/constants.js';
 import apiRouter from './routes/api.js';
+import dotenv from 'dotenv';
+import mysqlDb from './config/mysqlDb.js';
 
 const app: Express = express();
-
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter);
 
 const run = async () => {
+  await mysqlDb.init();
+
   app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
   });
